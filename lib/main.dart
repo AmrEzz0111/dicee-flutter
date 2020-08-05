47f58e52b6bb432836e22295e144009c2dd7cfc7
Amr Ezz <amrezz0111@gmail.com>
2020-08-05 19:14:16 +0200

    finished with the dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +11,65 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Dice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class Dice extends StatefulWidget {
+  Dice({Key key}) : super(key: key);
+
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int num1 = 0;
+  int num2 = 0;
+
+  void randome() {
+    num1 = Random().nextInt(5);
+    num2 = Random().nextInt(5);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(70.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    randome();
+                    print(num1);
+                  });
+                },
+                child: Image.asset('images/dice$num1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(70.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    randome();
+                    print(num2);
+                  });
+                },
+                child: Image.asset(
+                  'images/dice$num2.png',
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
